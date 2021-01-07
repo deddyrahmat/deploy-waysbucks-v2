43cbb868438257db2201ad53f7360c4d28d24a73
deddyrahmat <deddyrahmat97@gmail.com>
2021-01-07 19:57:00 +0700

    setting url in client

--- client/src/config/API/index.js
+++ client/src/config/API/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const API = axios.create({
-    baseURL: "http://localhost:5000/api/v1",
+    baseURL: "https://deploy-waysbucks.herokuapp.com/api/v1",
 });
 
 export const setAuthToken = (token) => {
